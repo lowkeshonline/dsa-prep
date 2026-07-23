2ce2963147a729e5999e3f77d3f81a81143b4286
Lokesh Chonga <143537645+lowkeshonline@users.noreply.github.com>
2026-07-23 22:15:32 +0530

    Time: 7 ms (28.01%), Space: 19.3 MB (73.46%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,40 +1,21 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-       
-        for row in range(9):
-            seen = set()
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-            for i in range(9):
-                if board[row][i] == ".":
+        row, col, square = defaultdict(set), defaultdict(set), defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+
+                if board[r][c] == '.':
                     continue
-                if board[row][i] in seen:
+                if (board[r][c] in row[r]
+                    or board[r][c] in col[c]
+                    or board[r][c] in square[(r // 3, c // 3)]):
                     return False
 
-                seen.add(board[row][i])
-        
-        for col in range(9):
-            seen = set()
-
-            for i in range(9):
-                    if board[i][col] == ".":
-                        continue
-                    if board[i][col] in seen:
-                        return False
-                    
-                    seen.add(board[i][col])
-
-        for square in range(9):
-            seen = set()
-            for i in range(3):
-                for j in range(3):
-                    row = (square // 3) * 3 + i
-                    col = (square % 3) * 3 + j
-
-                    if board[row][col] == ".":
-                        continue
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
+                col[c].add(board[r][c])
+                row[r].add(board[r][c])
+                square[( r // 3, c // 3)].add(board[r][c])
 
         return True
         
